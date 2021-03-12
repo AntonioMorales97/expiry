e5daee4bed66a9c0e84ecba93d5055877c78f81d
Gurk1992 <Gurk1992@users.noreply.github.com>
2021-03-12 22:53:07 +0100

    Fungerande flöde

--- hagrid/src/main/java/se/expiry/hagrid/HagridApplication.java
+++ hagrid/src/main/java/se/expiry/hagrid/HagridApplication.java
@@ -2,9 +2,38 @@ package se.expiry.hagrid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 
 @SpringBootApplication
 public class HagridApplication {
+	 static final String topicExchangeName = "notification";
+
+  static final String queueName = "";
+
+  @Bean
+  Queue queue() {
+    return new Queue(queueName, false);
+  }
+
+  	@Bean
+	DirectExchange exchange() {
+		return new DirectExchange(topicExchangeName);
+	}
+
+  @Bean
+  Binding binding(Queue queue, DirectExchange  exchange) {
+    return BindingBuilder.bind(queue).to(exchange).with("notify");
+  }
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HagridApplication.class, args);
--- hagrid/src/main/java/se/expiry/hagrid/presentation/controller/HelloWorld.java
+++ hagrid/src/main/java/se/expiry/hagrid/presentation/controller/HelloWorld.java
@@ -2,12 +2,17 @@ package se.expiry.hagrid.presentation.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class HelloWorld {
 
+    @Autowired
+	private AmqpTemplate amqpTemplate;
     @GetMapping("/hello")
     public String hej(){
-        return "Hello world";
+        amqpTemplate.convertAndSend("notification", "notify", "Hello World");
+        return "Message sent!";
     }
 }
