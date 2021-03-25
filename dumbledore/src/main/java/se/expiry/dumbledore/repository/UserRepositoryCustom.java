92d36a6bf42e036ff0fac651b854c84b5a59a318
Antonio <antonio_97@live.se>
2021-03-25 18:48:55 +0100

    multi module

--- dumbledore/src/main/java/se/expiry/dumbledore/repository/UserRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-package se.expiry.dumbledore.repository;
-
-import com.mongodb.client.result.UpdateResult;
-import se.expiry.dumbledore.domain.User;
-
-public interface UserRepositoryCustom {
-   User changePassword(String id, String email, String hashedPassword);
-}
