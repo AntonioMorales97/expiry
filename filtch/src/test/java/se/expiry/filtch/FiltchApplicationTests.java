2255cf913c9ad8c4811769ea9eaefc8cedfa2469
Antonio <antonio_97@live.se>
2021-05-03 19:44:40 +0200

    prep filtch for k8s and rename mongo to ron

--- filtch/src/test/java/se/expiry/filtch/FiltchApplicationTests.java
+++ filtch/src/test/java/se/expiry/filtch/FiltchApplicationTests.java
@@ -3,7 +3,6 @@ package se.expiry.filtch;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FiltchApplicationTests {
 
 	@Test
